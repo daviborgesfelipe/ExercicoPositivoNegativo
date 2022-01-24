a33a63cc4ef74c56c19f293ce1d5ba6455449f17
Davi Borges <davifelipe_300@hotmail.com>
2022-01-24 17:13:04 -0300

    Adicionando alguns arquivos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExercicoPositivoNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int numero;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero >= 0)
+            {
+                Console.WriteLine($"O número {numero} é positivo.");   
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} é negativo.");   
+            }
+        }
+    }
+}
